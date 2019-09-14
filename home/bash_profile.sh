cb68997868ed2dd234ba0858874adf2f1b3b02ee
dava <davkamail@gmail.com>
2019-09-14 19:24:09 +0100

    add welcome msg

--- home/bash_profile.sh
+++ home/bash_profile.sh
@@ -1,10 +1,27 @@
 #
 # ~/.bash_profile
 #
-
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
 
+#--------------------------------------------------------
+# ALIAS
+#--------------------------------------------------------
+alias p="pacman"
+alias sp="sudo pacman"
+
+
+#--------------------------------------------------------
+# MY ENVIRONMENT VARIABLE (must give absolute path)
+#--------------------------------------------------------
+export myworkspace="/home/dava/workspace"
+export myconfig="/home/dava/.1homepc-config"
+export myworkconfig="/home/dava/workspace/.work/winconfig"
+
+
+#--------------------------------------------------------
+# PATH
+#--------------------------------------------------------
 # Set our default path
 directory='/home/dava/.2shortcuts/directory'
 myfile='/home/dava/.2shortcuts/file'
@@ -12,11 +29,4 @@ execute='/home/dava/.2shortcuts/execute'
 system='/home/dava/.2shortcuts/system'
 launcher='/home/dava/.2shortcuts/launcher'
 rubyRails='/home/dava/.gem/ruby/2.3.0/bin'
-
 export PATH="${PATH}:/usr/bin/:$directory:$myfile:$execute:$system:$launcher:$rubyRails"
-
-# MY ENVIRONMENT VARIABLE (must give absolute path)
-export myworkspace="/home/dava/workspace"
-export myhomeconfig="/home/dava/.1homepc-config"
-export myworkconfig="/home/dava/workspace/.work/winconfig"
-export mywiki="/home/dava/.3installed/mywiki"
