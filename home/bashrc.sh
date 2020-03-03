f98f278363318fa855ebcd65824e0d64b8321c4b
Dava <davkamail@gmail.com>
2020-03-03 23:08:41 +0000

    add dmenu shortcut and extended installation files

--- home/bashrc.sh
+++ home/bashrc.sh
@@ -3,13 +3,13 @@
 #
 # If not running interactively, don't do anything
 [[ $- != *i* ]] && return
-alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
 
 
 #--------------------------------------------------------
 # ALIAS
 #--------------------------------------------------------
+alias ls='ls --color=auto'
 alias ll="ls -la"
 alias src="source ~/.bashrc"
 
