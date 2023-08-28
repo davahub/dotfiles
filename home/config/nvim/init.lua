21f691b9e8e422252849ea2eb3c1d671fd1c29b5
davahub <davkamail@gmail.com>
2023-08-28 17:42:34 +0100

    not working properly part 2

--- home/config/nvim/init.lua
+++ home/config/nvim/init.lua
@@ -1,30 +1,12 @@
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 vim.g.mapleader = ","
 
-require('config/keymaps')
+require("config.lazy")
 
-require('config/commands')
+-- require('config/keymaps')
 
-require('config/autocmd')
+-- require('config/commands')
 
-require("lazy").setup({
-  spec = {
-    { import = "plugins" }
-  },
-  change_detection = { enabled = false }
-})
+-- require('config/autocmd')
 
-require('config/options')
+-- require('config/options')
